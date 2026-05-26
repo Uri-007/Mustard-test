dd0851647b5bf6c766d9070b90e6d33c31d36355
Uri-007 <urielcesarsandoval10@gmail.com>
2026-05-26 16:38:51 -0600

    Ajustes de layout

--- src/constants/amenities.ts
+++ src/constants/amenities.ts
@@ -2,6 +2,22 @@
 import type { Amenity, AmenityImage } from "../types/animateImage.types";
 
 export const AMENITIES: Amenity[] = [
+  { icon: "mdi:controller-classic-outline", label: "LUDOTECA", imageIndex: 0 },
+  { icon: "mdi:pool", label: "ALBERCA", imageIndex: 1 },
+  { icon: "mdi:microphone-outline", label: "FORO", imageIndex: 2 },
+  { icon: "mdi:bookshelf", label: "BIBLIOTECA", imageIndex: 3 },
+  {
+    icon: "mdi:television-play",
+    label: "SALÓN DE USOS MÚLTIPLES",
+    imageIndex: 4,
+  },
+  {
+    icon: "mdi:account-group-outline",
+    label: "FAMILY ROOM",
+    imageIndex: 5,
+  },
+  { icon: "mdi:sofa-outline", label: "LOBBY", imageIndex: 6 },
+  { icon: "mdi:dumbbell", label: "GIMNASIO", imageIndex: 7 },
   { icon: "mdi:paw", label: "PET PARK", imageIndex: 8 },
   {
     icon: "mdi:fire",
@@ -9,18 +25,6 @@ export const AMENITIES: Amenity[] = [
     subLabel: "(EN NIVEL 20)",
     imageIndex: 9,
   },
-  { icon: "mdi:sofa-outline", label: "LOBBY", imageIndex: 6 },
-  { icon: "mdi:bookshelf", label: "BIBLIOTECA", imageIndex: 3 },
-  { icon: "mdi:controller-classic-outline", label: "LUDOTECA", imageIndex: 0 },
-  { icon: "mdi:account-group-outline", label: "FAMILY ROOM", imageIndex: 5 },
-  {
-    icon: "mdi:television-play",
-    label: "SALÓN DE USOS MÚLTIPLES",
-    imageIndex: 4,
-  },
-  { icon: "mdi:dumbbell", label: "GIMNASIO", imageIndex: 7 },
-  { icon: "mdi:pool", label: "ALBERCA", imageIndex: 1 },
-  { icon: "mdi:microphone-outline", label: "FORO", imageIndex: 2 },
 ];
 
 export const AMENITY_IMAGES: AmenityImage[] = [
@@ -32,7 +36,10 @@ export const AMENITY_IMAGES: AmenityImage[] = [
     src: "/src/assets/images/carrusel/salon.png",
     alt: "Salón de usos múltiples",
   },
-  { src: "/src/assets/images/carrusel/family room.png", alt: "Family Room" },
+  {
+    src: "/src/assets/images/carrusel/family room.png",
+    alt: "Family Room",
+  },
   { src: "/src/assets/images/carrusel/lobby.png", alt: "Lobby" },
   { src: "/src/assets/images/carrusel/gym.png", alt: "Gimnasio" },
   { src: "/src/assets/images/carrusel/pet.png", alt: "Pet Park" },
